6423ad2d265a659ca566354fb6bf8a72205c36ee
dahmie <omotayodamilare07@gmail.com>
2025-09-20 13:05:01 +0100

    add practice page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,16 +1,18 @@
-import HomeMain from "./main"
+import HomeMain from "./content"
 
 
 function HomeHeader(){
  return(
     <header className="bg-white sticky top-0 z-10 shadow-sm">
-          <div className="flex items-center p-4 justify-between">
-            <h1 className="text-xl font-bold text-gray-900">Home</h1>
+          <div className="flex items-center p-4 ">
             <button
               className="flex items-center justify-center rounded-full h-10 w-10 bg-gray-100 text-gray-600 hover:bg-gray-200"
             >
-              <span className="material-symbols-outlined text-2xl"> settings </span>
-            </button>
+              
+<svg height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
+            </button> 
+            <h1 className="flex text-xl text-center font-bold text-gray-900">Home</h1>
+             
           </div>
         </header>)
         
