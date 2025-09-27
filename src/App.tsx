14793552eb8cd1ece0ee225bff66b1aa3bf116aa
dahmie <omotayodamilare07@gmail.com>
2025-09-27 07:33:40 +0100

    add active link style to nav links

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter, Route, Routes, NavLink } from "react-router-dom";
 import HomePage from "./pages/home/home";
 import PracticePage from "./pages/practice/content";
 import "./App.css";
@@ -15,7 +15,18 @@ const NAVIGATION_ARRAY = [
         height="30px"
         viewBox="0 -960 960 960"
         width="30px"
-        fill="#1f1f1f"
+        fill="#1f1f1fff"
+      >
+        <path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z" />
+      </svg>
+    ),
+    icon2: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="30px"
+        viewBox="0 -960 960 960"
+        width="30px"
+        fill="#3ca4faff"
       >
         <path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z" />
       </svg>
@@ -37,6 +48,17 @@ const NAVIGATION_ARRAY = [
         <path d="M300-80q-58 0-99-41t-41-99v-520q0-58 41-99t99-41h500v600q-25 0-42.5 17.5T740-220q0 25 17.5 42.5T800-160v80H300Zm-60-267q14-7 29-10t31-3h20v-440h-20q-25 0-42.5 17.5T240-740v393Zm160-13h320v-440H400v440Zm-160 13v-453 453Zm60 187h373q-6-14-9.5-28.5T660-220q0-16 3-31t10-29H300q-26 0-43 17.5T240-220q0 26 17 43t43 17Z" />
       </svg>
     ),
+    icon2: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="30px"
+        viewBox="0 -960 960 960"
+        width="30px"
+        fill="#3ca4faff"
+      >
+        <path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z" />
+      </svg>
+    ),
   },
 ];
 
@@ -45,14 +67,17 @@ function NavFooter() {
     <footer className="sticky bottom-0 bg-white border-t border-gray-200 pb-safe">
       <div className="flex justify-around items-center h-16">
         {NAVIGATION_ARRAY.map((eachLink) => (
-          <Link
+          <NavLink
             className="flex flex-col items-center justify-center gap-1"
             to={eachLink.url}
             key={eachLink.id}
-          >
-            {eachLink.icon}
-            <p className="text-xs font-medium">{eachLink.pageName}</p>
-          </Link>
+            children={({ isActive }) => (
+              <>
+                {isActive ? eachLink.icon2 : eachLink.icon}
+                <p className="text-xs font-medium">{eachLink.pageName}</p>
+              </>
+            )}
+          ></NavLink>
         ))}
       </div>
     </footer>
