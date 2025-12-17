dafab6eef69282cba017d3ff7e1aaf02aa6da26c
dahmie <omotayodamilare07#gmail.com>
2025-12-17 16:23:26 +0000

    improve custom

---
+++ src/assets/icons/close.tsx
@@ -0,0 +1,13 @@
+export function CloseIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="15px"
+      viewBox="0 -960 960 960"
+      width="15px"
+      fill="#4a9c9cff"
+    >
+      <path d="M256-227.69 227.69-256l224-224-224-224L256-732.31l224 224 224-224L732.31-704l-224 224 224 224L704-227.69l-224-224-224 224Z" />
+    </svg>
+  );
+}
